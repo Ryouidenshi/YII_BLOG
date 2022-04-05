0c410ec2fbec45bf4b39293dd1dd86d33ad76717
Ryouidenshi <djonich@mail.ru>
2022-04-06 03:26:47 +0500

    add registration with vk or bd

--- views/auth/login.php
+++ views/auth/login.php
@@ -51,17 +51,15 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
 
         <div class="col-md-2">
-            <!-- Put this script tag to the <head> of your page -->
-            <script type="text/javascript" src="//vk.com/js/api/openapi.js?139"></script>
-
+            <script type="text/javascript" src="https://vk.com/js/api/openapi.js?169"></script>
             <script type="text/javascript">
-                VK.init({apiId: 5862316});
+                VK.init({apiId: 8128268});
             </script>
 
-            <!-- Put this div tag to the place, where Auth block will be -->
+            <!-- VK Widget -->
             <div id="vk_auth"></div>
             <script type="text/javascript">
-                VK.Widgets.Auth("vk_auth", {width: "200px", authUrl: '/auth/login-vk'});
+                VK.Widgets.Auth("vk_auth", {"authUrl":"/auth/login-vk"});
             </script>
         </div>
     </div>
--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -1,70 +1,77 @@
 <?php
 
-/** @var yii\web\View $this */
-/** @var string $content */
+/* @var $this \yii\web\View */
+/* @var $content string */
 
-use app\assets\AppAsset;
-use app\widgets\Alert;
-use yii\bootstrap4\Breadcrumbs;
-use yii\bootstrap4\Html;
 use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
+use yii\helpers\Html;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
 
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" class="h-100">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
 
-<header>
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/admin/default/index']],
-            ['label' => 'Articles', 'url' => ['/admin/article/index']],
-            ['label' => 'Categories', 'url' => ['/admin/category/index']],
-            ['label' => 'Tag', 'url' => ['/admin/tag/index']]
-        ],
-    ]);
-    NavBar::end();
-    ?>
-</header>
+    <div class="wrap">
+        <?php
+        NavBar::begin([
+            'brandLabel' => 'My Company',
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar-inverse navbar-fixed-top',
+            ],
+        ]);
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav navbar-right'],
+            'items' => [
+                ['label' => 'Home', 'url' => ['/admin/default/index']],
+                ['label' => 'Articles', 'url' => ['/admin/article/index']],
+                ['label' => 'Commentaries', 'url' => ['/admin/comment/index']],
+                ['label' => 'Categories', 'url' => ['/admin/category/index']],
+                ['label' => 'Tag', 'url' => ['/admin/tag/index']]
+            ],
+        ]);
+        NavBar::end();
+        ?>
 
-<main role="main" class="flex-shrink-0">
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= $content ?>
+        </div>
     </div>
-</main>
 
-<footer class="footer mt-auto py-3 text-muted">
-    <div class="container">
-        <p class="float-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="float-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+    <footer class="footer">
+        <div class="container">
+            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
 
-<?php $this->endBody() ?>
-</body>
-</html>
+            <p class="pull-right"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
+
+    <?php $this->endBody() ?>
+    <?php $this->registerJsFile('/ckeditor/ckeditor.js');?>
+    <?php $this->registerJsFile('/ckfinder/ckfinder.js');?>
+    <script>
+        $(document).ready(function(){
+            var editor = CKEDITOR.replaceAll();
+            CKFinder.setupCKEditor( editor );
+        })
+
+    </script>
+    </body>
+    </html>
 <?php $this->endPage() ?>
